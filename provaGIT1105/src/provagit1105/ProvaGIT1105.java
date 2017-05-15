11ce5d441a0bb6926bd400e257c100f8a98bebb0
professor <professor@professor-aula56>
2017-05-15 19:35:48 +0200

    Ver 1.1 He afegit una línea de signes =

--- provaGIT1105/src/provagit1105/ProvaGIT1105.java
+++ provaGIT1105/src/provagit1105/ProvaGIT1105.java
@@ -17,6 +17,7 @@ public class ProvaGIT1105 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("==================");
         
     }
     
